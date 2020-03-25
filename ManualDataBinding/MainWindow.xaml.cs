431670bac178c6eaada83e856a4e350318fb98e5
Thomas Reece <tommyr11@ksu.edu>
2020-03-24 23:23:42 -0600

    2way data binding

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,37 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        //New Note
+        Note note = new Note();
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+        /// <summary>
+        /// Random comment to get in the habit but this method initializes a new note
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+            
+        }
+        /// <summary>
+        /// Clears note
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+        }
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "Master Splinter";
+            note.Body = "There is no monster more dangerous than a lack of compassion";
         }
     }
 }
